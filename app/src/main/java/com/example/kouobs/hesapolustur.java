8a08c13d0a5e89a6d85f17590bcf07627e897bda
Arifyuce <82054323+Arifyuce@users.noreply.github.com>
2021-10-30 00:20:57 +0300

    arif1

--- app/src/main/java/com/example/kouobs/hesapolustur.java
+++ app/src/main/java/com/example/kouobs/hesapolustur.java
@@ -37,7 +37,7 @@ public class hesapolustur extends AppCompatActivity {
         EditText dogum = (EditText) findViewById(R.id.dogumtarihi);
 
 
-        //arif enemes
+        //arif enemesdfdf
 
 
 }
