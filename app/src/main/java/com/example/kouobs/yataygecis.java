b121f4f14bdf30995c90f8a5f40395c1553f6ba0
omergundogan <obgundogan.gmail.com>
2021-10-31 20:42:45 +0300

    duzeltme

---
+++ app/src/main/java/com/example/kouobs/yataygecis.java
@@ -0,0 +1,14 @@
+package com.example.kouobs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class yataygecis extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yataygecis);
+    }
+}
