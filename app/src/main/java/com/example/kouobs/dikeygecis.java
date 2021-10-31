1131b2f85462564bcb2eca6794648b7db4ff8f0f
Arifyuce <82054323+Arifyuce@users.noreply.github.com>
2021-10-31 23:11:56 +0300

    .

--- app/src/main/java/com/example/kouobs/dikeygecis.java
+++ app/src/main/java/com/example/kouobs/dikeygecis.java
@@ -3,6 +3,9 @@ package com.example.kouobs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class dikeygecis extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class dikeygecis extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dikeygecis);
+
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
     }
 }
