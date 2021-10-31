14bacec5875817e18de0a420d41d971dccd67875
omergundogan <obgundogan.gmail.com>
2021-10-31 22:20:07 +0300

    pdf viewer eklendi

--- app/src/main/java/com/example/kouobs/yataygecis.java
+++ app/src/main/java/com/example/kouobs/yataygecis.java
@@ -3,9 +3,34 @@ package com.example.kouobs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import android.os.AsyncTask;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.barteksc.pdfviewer.PDFView;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class yataygecis extends AppCompatActivity {
+
+    private Button btnDownload;
+    String URL = "http://www.codeplayon.com/samples/resume.pdf";
+
+    PDFView pdfView;
+
+    // url of our PDF file.
+    String pdfurl = "https://drive.google.com/drive/folders/179x3H_nE_93eqKyP_ibmveEkfnolmdJv";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +39,54 @@ public class yataygecis extends AppCompatActivity {
 
 
 
-        WebView myWebView = (WebView) findViewById(R.id.webview);
-        myWebView.loadUrl("https://developer.android.com/guide/webapps/webview#java");
+        // initializing our pdf view.
+        pdfView = findViewById(R.id.idPDFView);
+        new RetrivePDFfromUrl().execute(pdfurl);
+
+
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://drive.google.com/file/d/1XhfFo3WuW3Sak9PZnJvDOT_ga8UzPYpt/view?usp=sharing");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+
+    class RetrivePDFfromUrl extends AsyncTask<String, Void, InputStream> {
+        @Override
+        protected InputStream doInBackground(String... strings) {
+            // we are using inputstream
+            // for getting out PDF.
+            InputStream inputStream = null;
+            try {
+                URL url = new URL(strings[0]);
+                // below is the step where we are
+                // creating our connection.
+                HttpURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+                if (urlConnection.getResponseCode() == 200) {
+                    // response is success.
+                    // we are getting input stream from url
+                    // and storing it in our variable.
+                    inputStream = new BufferedInputStream(urlConnection.getInputStream());
+                }
+
+            } catch (IOException e) {
+                // this is the method
+                // to handle errors.
+                e.printStackTrace();
+                return null;
+            }
+            return inputStream;
+        }
+
+        @Override
+        protected void onPostExecute(InputStream inputStream) {
+            // after the execution of our async
+            // task we are loading our pdf in our pdf view.
+            pdfView.fromStream(inputStream).load();
+        }
+
+
     }
 }
