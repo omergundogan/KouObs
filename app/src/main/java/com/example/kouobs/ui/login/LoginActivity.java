509747aec9979b4c8e197f3860ad38498a972584
Arifyuce <82054323+Arifyuce@users.noreply.github.com>
2021-11-04 20:52:07 +0300

    kayit olduktan sonra giris ekranina aktariyor

--- app/src/main/java/com/example/kouobs/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/kouobs/ui/login/LoginActivity.java
@@ -132,11 +132,11 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              /*  loadingProgressBar.setVisibility(View.VISIBLE);
+               loadingProgressBar.setVisibility(View.VISIBLE);
                 loginViewModel.login(usernameEditText.getText().toString(),
                         passwordEditText.getText().toString());
 
-               */
+
 
                 Intent intent = new Intent (LoginActivity.this, AnaSayfa.class);
                 startActivity(intent);
