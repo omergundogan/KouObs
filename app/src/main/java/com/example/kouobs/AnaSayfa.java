a761cf2b89a2e90eaa79845c335707a648cfac67
haticealkap <haticealkap46@gmail.com>
2021-11-05 19:39:05 +0300

    giristen sonnraki ekrana kullanici idsi aktariliyor

--- app/src/main/java/com/example/kouobs/AnaSayfa.java
+++ app/src/main/java/com/example/kouobs/AnaSayfa.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class AnaSayfa extends AppCompatActivity {
+
 
 
 
@@ -16,8 +19,15 @@ public class AnaSayfa extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ana_sayfa);
 
-
+        TextView uid = (TextView) findViewById(R.id.userid);
         Button pdf = (Button) findViewById(R.id.pdf);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String value = extras.getString("userId");
+            Log.d("getuserid",value);
+           uid.setText(value);
+        }
 
         pdf.setOnClickListener(new View.OnClickListener() {
             @Override
